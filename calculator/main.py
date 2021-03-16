3c5ad1b014edd6a5b66e825b49dc377683d82e7e
qristjan <kristjanps96@gmail.com>
2021-03-16 21:36:14 +0200

    multiplication

--- calculator/main.py
+++ calculator/main.py
@@ -2,7 +2,10 @@ from matemaatika import matemaatika
 import re
 
 while True:
-    numbers = input("Enter add equation (like '3 + 5') or 'END' to stop the calculator: ")
+    numbers = input("Enter add/multiplication equation (like '3 + 5'/'3 * 5') or 'END' to stop the calculator: ")
     if str(numbers) == "END":
         exit(1)
-    print(matemaatika.liida(*[int(n) for n in re.sub(r"\s", "", numbers).split("+")]))
+    if "+" in str(numbers):
+        print(matemaatika.liida(*[int(n) for n in re.sub(r"\s", "", numbers).split("+")]))
+    if "*" in str(numbers):
+        print(matemaatika.korruta(*[int(n) for n in re.sub(r"\s", "", numbers).split("*")]))
--- matemaatika/matemaatika.py
+++ matemaatika/matemaatika.py
@@ -3,3 +3,7 @@ import functools
 
 def liida(*numbrid):
     return functools.reduce(lambda x, y: x + y, numbrid)
+
+
+def korruta(*numbrid):
+    return functools.reduce(lambda x, y: x * y, numbrid)
