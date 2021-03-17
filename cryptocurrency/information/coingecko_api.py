3a77234c5429b96df7a83e09cd2621324be64bee
qristjan <kristjanps96@gmail.com>
2021-03-17 22:58:36 +0200

    coiners

--- cryptocurrency/information/coingecko_api.py
+++ cryptocurrency/information/coingecko_api.py
@@ -1,7 +1,7 @@
 import requests
 
 
-class Coin:
+class CoinListItem:
     def __init__(self, coin_id, symbol, name):
         self.coin_id = coin_id
         self.symbol = symbol
@@ -15,6 +15,25 @@ class Coin:
         return self.name
 
 
+class Coin:
+    def __init__(self, coin_id, name, price):
+        self.coin_id = coin_id
+        self.name = name
+        self.price = price
+
+    @classmethod
+    def from_json(cls, json):
+        return cls(json['id'], json['name'], json['market_data']['current_price']['eur'])
+
+    def __str__(self):
+        return "{}:\n{} (EUR)".format(self.name, self.price)
+
+
 def list_coins():
     r = requests.get('https://api.coingecko.com/api/v3/coins/list')
-    return [Coin.from_json(coin_info) for coin_info in r.json()]
+    return [CoinListItem.from_json(coin_info) for coin_info in r.json()]
+
+
+def get_coin_info(coin):
+    r = requests.get('https://api.coingecko.com/api/v3/coins/{}'.format(coin.coin_id))
+    return Coin.from_json(r.json())
--- cryptocurrency/information/window.py
+++ cryptocurrency/information/window.py
@@ -1,5 +1,37 @@
 import cryptocurrency.information.coingecko_api as ca
+import tkinter as tk
 
-coin_info = ca.list_coins()
+coin_info = {coin.name: coin for coin in ca.list_coins()}
 
-[print(info) for info in coin_info]
+top = tk.Tk()
+top.title("Coingecko")
+top.geometry("300x500")
+
+listbox = tk.Listbox(top)
+listbox.pack(expand=1, side=tk.LEFT, fill=tk.BOTH)
+scrollbar = tk.Scrollbar(top, orient=tk.VERTICAL)
+scrollbar.config(command=listbox.yview)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+listbox.config(yscrollcommand=scrollbar.set)
+
+
+def open_coin_window(coin):
+    coin_top = tk.Toplevel(top)
+    coin_top.title(coin.name)
+    coin_top.geometry("300x300")
+    info = ca.get_coin_info(coin)
+    coin_text = tk.Text(coin_top, height=5, width=100)
+    coin_text.pack()
+    coin_text.insert(tk.END, info)
+
+
+def select(evt):
+    open_coin_window(coin_info[listbox.get(tk.ANCHOR)])
+
+
+listbox.bind('<<ListboxSelect>>', select)
+
+for ci in coin_info.keys():
+    listbox.insert(tk.END, ci)
+
+top.mainloop()
